f173a3af16fe7338f6e68672b8096b197b9abd8f
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-09-14 17:24:10 +0200

    tester commit

---
+++ Gruppeoppgave1_Webapplikasjoner/Controllers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppeoppgave1_Webapplikasjoner.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class KundeController : ControllerBase
+    {
+       //sjekke om det funker å pushe 
+    }
+} 
---
+++ Gruppeoppgave1_Webapplikasjoner/Models.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Gruppeoppgave1_Webapplikasjoner.Models
+{
+    public class Billett
+    {
+        public int Id { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Telefonnr { get; set; }
+        public string Mail { get; set; }
+        public string Adresse { get; set; }
+        public string Postnr { get; set; }
+        public string Poststed { get; set; }
+    } 
+}
+
+
