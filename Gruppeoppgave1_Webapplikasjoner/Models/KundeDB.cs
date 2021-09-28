23f21811b713c1120a6d8329b4c48a9de39fc621
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-09-28 09:42:55 +0200

    Prøvde en ny database "Billett.db" og endret KundeDB. fortsatt problemer

--- Gruppeoppgave1_Webapplikasjoner/Models/KundeDB.cs
+++ Gruppeoppgave1_Webapplikasjoner/Models/KundeDB.cs
@@ -7,14 +7,14 @@ using Microsoft.EntityFrameworkCore;
 namespace Gruppeoppgave1_Webapplikasjoner.Models
 {
     public class KundeDB : DbContext 
-                //Her skal "DbContext" bli samme farge som "KundeDB"
+                
     {
         public KundeDB (DbContextOptions<KundeDB> options) : base(options)
-            //"DbContextOptions" skal ogs? endre farge egentlig
+            
         {
             Database.EnsureCreated();
-        } //Hele denne skal endre farger
+        } 
 
-        public DbSet<Kunde> Kunder { get; set; }
+        public DbSet<Billett> Kunder { get; set; } // Tror dette blir riktig istedet for "Kunde". Vi har jo alle datapunktene under "Billett" ikke "kunde"..
     }
 }
