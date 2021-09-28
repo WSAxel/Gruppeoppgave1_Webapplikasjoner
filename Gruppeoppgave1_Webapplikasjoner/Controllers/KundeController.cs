4bf410b1c2810b87573a67fef4b5dc4b6ffbcf00
sheima <s338853@oslomet.no>
2021-09-28 18:53:54 +0200

    * Startup.cs: * Billett.cs: * KundeDB.cs: * KundeController.cs:
    * Kunde.cs: database, ikke helt ferdig

--- Gruppeoppgave1_Webapplikasjoner/Controllers/KundeController.cs
+++ Gruppeoppgave1_Webapplikasjoner/Controllers/KundeController.cs
@@ -16,27 +16,74 @@ namespace Gruppeoppgave1_Webapplikasjoner.Controllers
             _kundeDB = kundeDb;
         }
 
+        public void SettInn(Billett bestiltBillett)
+        {
+            var bestilling = new Bestilling()
+            {
+                AntallBarn = bestiltBillett.AntallBarn,
+                AntallVoksne = bestiltBillett.AntallVoksne,
+                Avreise = bestiltBillett.Avreise
+
+            };
+
+            Kunde funnetKunde = _kundeDB.Kunder.FirstOrDefault(k => k.Fornavn == bestiltBillett.Fornavn);
+
+            if(funnetKunde == null) {
+                //oprett kunde
+                var kunde = new Kunde
+                {
+                    Fornavn = bestiltBillett.Fornavn,
+                    Etternavn = bestiltBillett.Etternavn,
+                    Telefonnr = bestiltBillett.Telefonnr,
+                    Mail = bestiltBillett.Mail,
+                    Adresse = bestiltBillett.Adresse,
+                    Postnr = bestiltBillett.Postnr,
+                    Poststed = bestiltBillett.Poststed
+                };
+                //legg bestilling inn i kunde
+                kunde.Bestillinger = new List<Bestilling>();
+                kunde.Bestillinger.Add(bestilling);
+                _kundeDB.Kunder.Add(kunde);
+                _kundeDB.SaveChanges();
+            }
+            else
+            {
+                funnetKunde.Bestillinger.Add(bestilling);
+                _kundeDB.SaveChanges();
+            }
+        }
+
         public List<Kunde> HentAlle()
         {
             List<Kunde> alleKundene = _kundeDB.Kunder.ToList();
+
+            List<Billett> alleBestillinger = new List<Billett>();
+            foreach (var kunde in alleKundene)
+            {
+                foreach (var best in kunde.Bestillinger)
+                {
+                    var enBestilling = new Billett
+                    {
+                        Fornavn = kunde.Fornavn,
+                        Etternavn = kunde.Etternavn,
+                        Telefonnr = kunde.Telefonnr,
+                        Mail = kunde.Mail,
+                        Adresse = kunde.Adresse,
+                        Postnr = kunde.Postnr,
+                        Poststed = kunde.Poststed,
+
+                        AntallBarn = best.AntallBarn,
+                        AntallVoksne = best.AntallVoksne,
+                        Avreise = best.Avreise
+
+                    };
+                    alleBestillinger.Add(enBestilling);
+                }
+            }
             return alleKundene;
         }
 
-        public bool Lagre(Kunde innKunde)
-        {
-            try
-            {
-                _kundeDB.Kunder.Add(innKunde);
-                _kundeDB.SaveChanges();
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-           
-            
-        }
+       
         
     }
 }
--- Gruppeoppgave1_Webapplikasjoner/Models/KundeDB.cs
+++ Gruppeoppgave1_Webapplikasjoner/Models/KundeDB.cs
@@ -1,11 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Gruppeoppgave1_Webapplikasjoner.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gruppeoppgave1_Webapplikasjoner.Models
 {
+    public class Kunde
+    {
+        [Key]
+        public int KId { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Adresse { get; set; }
+        public string Postnr { get; set; }
+        public string Poststed { get; set; }
+        public string Telefonnr { get; set; }
+        public string Mail { get; set; }
+
+        public virtual List<Bestilling> Bestillinger { get; set; }
+
+    }
+
+    public class Bestilling
+    {
+        [Key]
+        public int BId { get; set; }
+        public int AntallBarn { get; set; }
+        public int AntallVoksne { get; set; }
+        public string Avreise { get; set; }
+    }
+
+}
     public class KundeDB : DbContext 
                 
     {
@@ -15,6 +43,6 @@ namespace Gruppeoppgave1_Webapplikasjoner.Models
             Database.EnsureCreated();
         } 
 
-        public DbSet<Billett> Kunder { get; set; } // Tror dette blir riktig istedet for "Kunde". Vi har jo alle datapunktene under "Billett" ikke "kunde"..
-    }
+        public DbSet<Kunde> Kunder { get; set; }
+        public DbSet<Bestilling> Bestillinger { get; set; }
 }
