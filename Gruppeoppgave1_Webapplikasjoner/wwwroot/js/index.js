42829437b24fd7d51c4171e220716b0414387661
Lokastico <56114098+Lokastico@users.noreply.github.com>
2021-09-30 13:22:06 +0200

    Endret index.js linje 25

--- Gruppeoppgave1_Webapplikasjoner/wwwroot/js/index.js
+++ Gruppeoppgave1_Webapplikasjoner/wwwroot/js/index.js
@@ -22,7 +22,7 @@ function lagreBestilling() {
         adresse: $("#adresse").val(),
         antallBarn: $("#antallBarn").val()
     }
-    const url = "Billett/SettInn";
+    const url = "Kunde/SettInn";
     $.post(url, billett, function (OK) {
         if (OK) {
             //window.location.href = "index.html"
